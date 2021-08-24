43ec4961b6dcd8ca82f39ffd23e4ee843a0b5805
NatalyMora21 <tatamora21gmail.com>
2021-08-24 20:00:50 +0200

    Creation of new schemas to query buyer data

--- Server/schema/typeEntity.go
+++ Server/schema/typeEntity.go
@@ -7,10 +7,11 @@ type Buyer struct {
 }
 
 type Product struct {
-	Id    string `json:"id"`
-	Name  string `json:"name"`
-	Price int    `json:"price"`
-	Uid   string `json:"uid"`
+	Id    string   `json:"id"`
+	Name  string   `json:"name"`
+	Price int      `json:"price"`
+	Uid   string   `json:"uid"`
+	DType []string `json:"dgraph.type,omitempty"`
 }
 
 type Transaction struct {
@@ -27,6 +28,16 @@ type TransactionProduct struct {
 	Ip       string      `json:"ip"`
 	Device   string      `json:"device"`
 	Products []IdProduct `json:"products"`
+	DType    []string    `json:"dgraph.type,omitempty"`
+}
+
+type Transactionj struct {
+	Id       string    `json:"id"`
+	Buyer_id string    `json:"buyer_id"`
+	Ip       string    `json:"ip"`
+	Device   string    `json:"device"`
+	Products []Product `json:"products"`
+	DType    []string  `json:"dgraph.type,omitempty"`
 }
 
 type IdProduct struct {
@@ -40,4 +51,14 @@ type InfoBuyer struct {
 	Name         string               `json:"name"`
 	Age          int                  `json:"age"`
 	Transactions []TransactionProduct `json:"transactions"`
+	DType        []string             `json:"dgraph.type,omitempty"`
+}
+
+type InfoBuyerj struct {
+	Uid          string         `json:"uid"`
+	Id           string         `json:"id"`
+	Name         string         `json:"name"`
+	Age          int            `json:"age"`
+	Transactions []Transactionj `json:"transactions"`
+	DType        []string       `json:"dgraph.type,omitempty"`
 }
