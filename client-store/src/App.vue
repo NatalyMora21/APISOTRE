0316e6a99550d2ff81c91cedd46257e769806552
NatalyMora21 <tatamora21gmail.com>
2021-08-25 03:15:48 +0200

    Creada esquema de carpetas de interfaz visual

---
+++ client-store/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Age</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="buyer in buyers" :key="buyer.id"> 
+
+          <td>{{buyer.id}}</td>
+          <td>{{buyer.name}}</td>
+          <td>{{buyer.age}}</td>
+        
+        </tr>
+      </tbody>
+    </table>
+  
+  </div>
+
+</template>
+
+<script>
+
+  import axios from 'axios'
+
+
+  export default {
+    name: 'App',
+    data(){
+      return {
+        buyers: null
+      }
+    },
+    mounted(){
+      this.getAllBuyers();
+      
+    },
+    methods: {
+     async getAllBuyers (){
+        console.log("Todos Buyers")
+        try{
+          this.buyers= await (await axios.get("http://localhost:4000/")).data;
+          console.log(this.buyers.data)      
+
+        }catch(err){
+          console.log(err)
+        }
+
+      }
+    }
+
+  }
+</script>
+
+<style>
+
+</style>
